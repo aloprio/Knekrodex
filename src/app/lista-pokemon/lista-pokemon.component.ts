3010b2439cc52f594f87937aad64731edd2a8351
AntoñitoElMetralleta <93702741+aloprio@users.noreply.github.com>
2023-11-08 14:05:41 +0100

    Implementada api con servicio de pokemons
    Faltan ajustes solo muestra nombres de pokemon

--- src/app/lista-pokemon/lista-pokemon.component.ts
+++ src/app/lista-pokemon/lista-pokemon.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonsService } from '../pokemons.service';
 
 @Component({
   selector: 'app-lista-pokemon',
   templateUrl: './lista-pokemon.component.html',
   styleUrls: ['./lista-pokemon.component.css']
 })
-export class ListaPokemonComponent {
+export class ListaPokemonComponent implements OnInit {
+
+  pokemonsarray: any[] = []
+
+  constructor(private pokemonsService: PokemonsService) {}
+
+  ngOnInit(): void {
+    this.pokemonsService.getPokemons().subscribe((data: any) => { this.pokemonsarray = data.results});
+  }
 
 }
