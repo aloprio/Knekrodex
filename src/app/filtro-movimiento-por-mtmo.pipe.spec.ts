3c33131301e182bb65110f2600846f2af8d981a2
aloprio <93702741+aloprio@users.noreply.github.com>
2023-12-11 16:17:27 +0100

    Deploy

--- src/app/filtro-movimiento-por-mtmo.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FiltroMovimientoPorMTMOPipe } from './filtro-movimiento-por-mtmo.pipe';
-
-describe('FiltroMovimientoPorMTMOPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FiltroMovimientoPorMTMOPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/filtro-movimientos-por-nivel.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FiltroMovimientosPorNivelPipe } from './filtro-movimientos-por-nivel.pipe';
-
-describe('FiltroMovimientosPorNivelPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FiltroMovimientosPorNivelPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
